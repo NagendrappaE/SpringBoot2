c29ece28b3dac466a5c3e32538d61d194f9a3607
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-08-05 13:51:50 +0530

    spring login

---
+++ SpringBoot-login/src/test/java/com/ece/boot/login/SpringBootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.boot.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
